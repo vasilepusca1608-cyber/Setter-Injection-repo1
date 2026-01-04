07d5859cccb1efe6d660d71fa5c590ea67690dc9
Pusca Vasile <vasilepusca1608@gmail.com>
2026-01-04 12:45:51 +0200

    New commit application

--- src/main/java/pixel/academy/setter_injection/common/TurkishChef.java
+++ src/main/java/pixel/academy/setter_injection/common/TurkishChef.java
@@ -1,8 +1,10 @@
 package pixel.academy.setter_injection.common;
 
+import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Component;
 
 @Component
+@Lazy
 public class TurkishChef implements Chef {
 
     public TurkishChef() {
